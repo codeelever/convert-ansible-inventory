db6ff1843944798e6d71ccc4f2156a8b0154b539
Chris Burnett <codeelver@outlook.com>
2020-07-25 08:31:34 -0400

    Change sample import to BeforeEach
    This removes the need to import csv sample data in each\
    'It' test scope.

--- ConvertTo-AnsibleInventory.Tests.ps1
+++ ConvertTo-AnsibleInventory.Tests.ps1
@@ -4,16 +4,16 @@ $modules = "$here\*" | gci -include '*.psm1'
 $modules |  Import-Module -Force
 
 Describe "ConvertTo-AnsibleInventory" {
-    $obj = Import-CSV "inventory.csv" | Select-Object -First 1 
+    BeforeEach {
+      $obj = Import-CSV "inventory.csv" | Select-Object -First 1    
+    }
        
     It "Creates Inventory Group" {
-       $obj = Import-CSV "inventory.csv" | Select-Object -First 1
        Create-InventoryGroup $obj | Should -Be "[group1-test-app]"
 
     }  
     
     It "Creates Inventory Host" {
-      $obj = Import-CSV "inventory.csv" | Select-Object -First 1
       Create-InventoryHost $obj | Should -Be "group1-test-web-1 ansible_host=x.x.x.x"
     }
 
