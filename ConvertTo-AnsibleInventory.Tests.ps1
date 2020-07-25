ef1fd4b65253765591a43996af7dc4b5503c9135
Chris Burnett <codeelver@outlook.com>
2020-07-25 08:38:10 -0400

    Update test object name

--- ConvertTo-AnsibleInventory.Tests.ps1
+++ ConvertTo-AnsibleInventory.Tests.ps1
@@ -5,16 +5,16 @@ $modules |  Import-Module -Force
 
 Describe "ConvertTo-AnsibleInventory" {
     BeforeEach {
-      $obj = Import-CSV "sample-inventory.csv" | Select-Object -First 1    
+      $testobj = Import-CSV "sample-inventory.csv" | Select-Object -First 1    
     }
        
     It "Creates Inventory Group" {
-       Create-InventoryGroup $obj | Should -Be "[group1-test-app]"
+       Create-InventoryGroup $testobj | Should -Be "[group1-test-app]"
 
     }  
     
     It "Creates Inventory Host" {
-      Create-InventoryHost $obj | Should -Be "group1-test-web-1 ansible_host=x.x.x.x"
+      Create-InventoryHost $testobj | Should -Be "group1-test-web-1 ansible_host=x.x.x.x"
     }
 
 }
